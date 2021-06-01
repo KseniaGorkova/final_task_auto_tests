f150768405057fe380e5288075739a679d156892
Манка <manka@MacBook-Pro-Oleg-2.local>
2021-06-01 20:56:38 +0300

    add_login_page

--- test_login_page.py
+++ test_login_page.py
@@ -6,5 +6,6 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser,link)
     page.open()
     page.go_to_login_page()
-    #
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
     
